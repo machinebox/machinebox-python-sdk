fe523f1df3aa632d0459c6d0e761847fa5452ecf
robmarkcole <robmarkcole@gmail.com>
2018-11-27 07:42:33 +0000

    tidy

--- tests/context.py
+++ tests/context.py
@@ -4,6 +4,6 @@ Import paths for tests.
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..', 'facebox')))
+    os.path.dirname(__file__), '..', 'machinebox')))
 
 import facebox
